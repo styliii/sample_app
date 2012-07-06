0aef0beae2f9367c445134a515748e671820a0ae
Li Ouyang <li.ouyang@gmail.com>
2012-07-06 00:06:01 -0400

    make rspec tests prettier

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,45 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+describe "StaticPages" do  
   
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',     text: heading) }
+    it { should have_selector('title',  text: full_title(page_title)) }
+  end
+  
   describe "Home page" do
     before {visit root_path }
+    let(:heading)     { 'Sample App' }
+    let(:page_title)  { '' }
     
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector 'title', text: full_title('') }
-    it {should_not have_selector 'title', text: '| Home' }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
   end
   
   describe "Help page" do
     before { visit help_path }
+    let(:heading)   { 'Help' }
+    let(:page_title)  { 'Help' }
     
-    it { should have_selector 'h1', text: 'Help' }
-    it {should have_selector 'title', text: full_title('Help') }
+    it_should_behave_like "all static pages"
   end
   
   describe "About page" do
     before { visit about_path }
-    it { should have_selector 'h1', text: 'About Us'}
-    it { should have_selector 'title', text: full_title('About') }
+    let(:heading)   { 'About' }
+    let(:page_title)  { 'About' }
+    
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_selector 'h1', text: 'Contact'}
-    it { should have_selector 'title', text: full_title('Contact') }
+    let(:heading)   { 'Contact' }
+    let(:page_title)     { 'Contact' }
+    
+    it_should_behave_like "all static pages"
   end
 
 end
