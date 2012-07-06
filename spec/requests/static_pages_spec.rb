d32a7cc280a963e90c83df122b9c55394feedae6
Li Ouyang <li.ouyang@gmail.com>
2012-07-06 12:03:22 -0400

    ex 1-3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,19 @@ describe "StaticPages" do
     it_should_behave_like "all static pages"
   end
 
+  it "should have all the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+  end
+
 end
