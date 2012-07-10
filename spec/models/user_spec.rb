3e3ad375fba0f69c523eb6726722ea322f336744
Li Ouyang <li.ouyang@gmail.com>
2012-07-10 12:11:14 -0400

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+    it { should respond_to(:authenticate) }
   
   it { should be_valid }
   
@@ -79,18 +80,24 @@ describe User do
   
   describe "return value of authenticate method" do
     before { @user.save }
-    let(:found_user) { User.find_by_emai(@user.email) }
+    let(:found_user) { User.find_by_email(@user.email) }
     
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
-    
+  
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-      
+    
       it {should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+  
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" *5 }
+    it { should be_invalid}
+  end
 
 end
 # == Schema Information
